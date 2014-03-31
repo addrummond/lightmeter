5908e0cbd061827676bc63ec0397284399058314
Alex Drummond <a.d.drummond@gmail.com>
2014-03-30 22:24:01 -0400

    Switch to using a timed interrupt for ADC readings (about 8 per second)

--- lightmeter.c
+++ lightmeter.c
@@ -17,7 +17,9 @@
 // Save result of ADC on interrupt.
 volatile static uint16_t adc_value = 0;
 ISR(ADC_vect) {
+    TIFR |= (1<<OCF0A); // Clear timer compare match flag.
     adc_value = ADCW;
+    ADCSRA |= (1 << ADSC);
 }
 
 void setupADC()
@@ -26,12 +28,22 @@ void setupADC()
     ADMUX = (0 << REFS1) | (0 << REFS0);
     // Set differential ADC with PB4 pos, PB3 neg, and 1x gain (see atiny85 datasheet p. 135).
     ADMUX |= (0 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0); // 0110
-    // Auto triggering (this needs to be set for turning on free running in ADCSRB to take effect).
+    // Auto triggering (this needs to be set for turning on counter interrupt ADCSRB to take effect).
     ADCSRA |= (1 << ADATE);
     // Enable ADC interrupt.
     ADCSRA |= (1 << ADIE);
-    // Free running mode.
+    // Timer/counter 0 compare match A setup.
+    // This causes the ADC to fire on an interupt once every 1024 clock cycles
+    // (so about 8 times a second if we're running at 8MHz).
     ADCSRB &= ~((1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0));
+    ADCSRB &= ~((1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0));
+    ADCSRB |= ((0 << ADTS2) | (1 << ADTS1) | (1 << ADTS0)); // Timer/Counter0 Compare Match A
+    TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00));
+    TCCR0B |= ((1 << CS02) | (0 << CS01) | (0 << CS00)); // prescaler: clock/256
+    TCCR0A &= ~((1 << COM0A1) | (1 << COM0A0));
+    TCCR0A |= (1 << COM0A1 | (0 << COM0A0)); // Clear OC0A/OC0B on Compare Match
+    TCNT0 = 0;
+    OCR0A = 4;
     // Turn off bipolar input mode.
     ADCSRB &= ~(1 << BIN);
     // Sets prescaler to 128, which at 8MHz gives a 62KHz ADC.
@@ -56,11 +68,15 @@ void setupOutputPorts()
 
 void handleMeasurement()
 {
-    uint8_t v = convert_from_reference_voltage(adc_value);
-    uint8_t ev = get_ev100_at_temperature_voltage(178/*20C*/, v);
-    uint8_t outOfEight = ev / 8;
+    // Copy the volatile value, which could change in the middle of this function.
+    uint16_t adc_nonvol_value = adc_value;
 
-    uint8_t led = ((outOfEight & 0b100)>> 2) | (outOfEight & 0b010) | ((outOfEight & 0b001) << 2);
+    uint8_t v = convert_from_reference_voltage(adc_nonvol_value);
+    uint8_t ev = get_ev100_at_temperature_voltage(178/*20C*/, v);
+    // Bit of a hack.
+    uint8_t out_of_eight = (ev - 100) / 16;
+
+    uint8_t led = ((out_of_eight & 0b100)>> 2) | (out_of_eight & 0b010) | ((out_of_eight & 0b001) << 2);
     PORTB &= ~(0b111);
     PORTB |= led;
 }
