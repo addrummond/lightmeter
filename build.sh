a8bc0e44d8d26d12bd8b292609ad4b14c5f565a4
Alex Drummond <a.d.drummond@gmail.com>
2014-03-30 22:44:42 -0400

    Add led_debug.c/h

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ python calculate_tables.py > tables.c
 
 avr-gcc $CFLAGS -o tables.out -c tables.c
 avr-gcc $CFLAGS -o calculate.out -c calculate.c
+avr-gcc $CFLAGS -o led_debug.out -c led_debug.c
 avr-gcc $CFLAGS -o lightmeter.out -c lightmeter.c
-avr-gcc $CFLAGS calculate.out tables.out lightmeter.out -o main.out
+avr-gcc $CFLAGS calculate.out tables.out led_debug.out lightmeter.out -o main.out
 avr-objcopy -O ihex -R .eeprom main.out lightmeter.hex
 cp lightmeter.hex /tmp
---
+++ led_debug.c
@@ -0,0 +1,26 @@
+#include <led_debug.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Display a byte value using three LEDs.
+void debug_led_show_byte(uint8_t byte)
+{
+    uint8_t leds[3] = { byte >> 5, (byte & 0b00011100) >> 2, byte & 0b00000011 };
+
+    // Two brief flashes of all the LEDs to signal the start of the value.
+    PORTB |= ((1 << LED_LEFT_PB | (1 << LED_MIDDLE_PB) | (1 << LED_RIGHT_PB)));
+    _delay_ms(100);
+    PORTB &= ~((1 << LED_LEFT_PB | (1 << LED_MIDDLE_PB) | (1 << LED_RIGHT_PB)));
+    _delay_ms(100);
+    PORTB |= ((1 << LED_LEFT_PB | (1 << LED_MIDDLE_PB) | (1 << LED_RIGHT_PB)));
+    _delay_ms(100);
+
+    int i;
+    for (i = 0; i < 3; ++i) {
+        PORTB &= ~((1 << LED_LEFT_PB | (1 << LED_MIDDLE_PB) | (1 << LED_RIGHT_PB)));
+        PORTB |= (((leds[i] & 4) << LED_RIGHT_PB) |
+                  ((leds[i] & 2) << LED_MIDDLE_PB) |
+                  ((leds[i] & 1) << LED_LEFT_PB));
+        _delay_ms(100);
+    }
+}
