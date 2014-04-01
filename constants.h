d965ba226d54dc333123dd7be06405e0b22a88d2
Alex Drummond <a.d.drummond@gmail.com>
2014-04-01 02:18:56 -0400

    Some initial work on using temperature readings

--- constants.h
+++ constants.h
@@ -1,1 +1,7 @@
 #define REFERENCE_VOLTAGE_MV 2500
+
+// See p. 133 of atiny85 data sheet.
+// TODO: At some point these values should be
+// moved to EPROM so that they can be set differently for each chip
+// without recompiling.
+#define ADC_TEMPERATURE_OFFSET 0
--- lightmeter.c
+++ lightmeter.c
@@ -6,6 +6,7 @@
 
 #include <constants.h>
 #include <calculate.h>
+#include <led_debug.h>
 
 /*
                          1 ---- 8         VCC
@@ -14,20 +15,43 @@
             GND          4 ---- 5 (PB0)   LED left    (most sig)
  */
 
+const uint8_t ADMUX_CLEAR_SOURCE = ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+const uint8_t ADMUX_LIGHT_SOURCE = (0 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0); // 0110
+const uint8_t ADMUX_TEMPERATURE_SOURCE = (1 << MUX3) | (1 << MUX 2) | (1 << MUX1) | (1 << MUX0);
+
 // Save result of ADC on interrupt.
-volatile static uint16_t adc_value = 0;
+volatile static uint16_t adc_light_value = 0;
+volatile static uint16_t adc_temperature_value = 300; // About 25C
 ISR(ADC_vect) {
+    static uint8_t counter = 1;
+
+    if (counter == 0)
+        // Next time read the temperature, not the light level.
+        ADMUX |= ADMUX_TEMPERATURE_SOURCE; // 1111, so no need to clear bits first.
+
+    if (counter == 1) {
+        // We're reading the temperature.
+        adc_temperature_value = ADCW + ADC_TEMPERATURE_OFFSET;
+        // Set it back to measuring light level for next time.
+        ADMUX |= ADMUX_LIGHT_SOURCE;
+    }
+    else {
+        // We're reading the light level.
+        adc_light_value = ADCW;
+    }
+
     TIFR |= (1<<OCF0A); // Clear timer compare match flag.
-    adc_value = ADCW;
     ADCSRA |= (1 << ADSC);
+
+    counter += 2; // Will overflow back to 0 eventually.
 }
 
-void setupADC()
+void setup_ADC()
 {
     // Set ref voltage to VCC.
     ADMUX = (0 << REFS1) | (0 << REFS0);
     // Set differential ADC with PB4 pos, PB3 neg, and 1x gain (see atiny85 datasheet p. 135).
-    ADMUX |= (0 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0); // 0110
+    ADMUX |= ADMUX_TEMPERATURE_SOURCE;
     // Auto triggering (this needs to be set for turning on counter interrupt ADCSRB to take effect).
     ADCSRA |= (1 << ADATE);
     // Enable ADC interrupt.
@@ -58,27 +82,27 @@ void setupADC()
     sei();
 }
 
-void setupOutputPorts()
+void setup_output_ports()
 {
     DDRB = 0b00000111; // Set PB2 - PB0 as output ports.
 }
 
-void handleMeasurement()
+void handle_measurement()
 {
     // Copy the volatile value, which could change in the middle of this function.
-    uint16_t adc_nonvol_value = adc_value;
+    uint16_t adc_light_nonvol_value = adc_light_value;
 
-    uint8_t v = convert_from_reference_voltage(adc_nonvol_value);
-    uint8_t ev = get_ev100_at_temperature_voltage(178/*20C*/, v);
+    uint8_t v = convert_from_reference_voltage(adc_light_nonvol_value);
+    uint8_t ev = get_ev100_at_temperature_voltage(178, v); // 128 = 20C
     // Bit of a hack.
     uint8_t out_of_eight = (ev - 100) / 16;
 
-    uint8_t led = ((out_of_eight & 0b100)>> 2) | (out_of_eight & 0b010) | ((out_of_eight & 0b001) << 2);
+    uint8_t led = ((out_of_eight & 0b100) >> 2) | (out_of_eight & 0b010) | ((out_of_eight & 0b001) << 2);
     PORTB &= ~(0b111);
-    PORTB |= led;
+    PORTB |= led;*/
 }
 
-void ledTest()
+void led_test()
 {
     _delay_ms(500);
     PORTB |= 0b1;
@@ -95,14 +119,14 @@ void ledTest()
 
 int main()
 {
-    setupOutputPorts();
-    ledTest();
+    setup_output_ports();
+    led_test();
 
     _delay_ms(1000);
 
-    setupADC();
+    setup_ADC();
     for (;;) {
-        handleMeasurement();
+        handle_measurement();
         _delay_ms(250);
     }
 
