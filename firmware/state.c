821163c6862984406e3edf00c32da3c5aee8febf
Alex Drummond <a.d.drummond@gmail.com>
2014-04-06 23:14:39 -0400

    Remove old in-code initialization of meter state

--- firmware/state.c
+++ firmware/state.c
@@ -3,21 +3,7 @@
 
 #include <avr/eeprom.h>
 
-meter_state_t global_meter_state = {
-    { 0, 0, 0, 0, 1, 0, 0 },   // bcd_iso
-    3,                         // bcd_iso_length
-    4,                         // stops_iso
-
-    SHUTTER_PRIORITY,
-
-    48,                        // aperture (f8)
-    96,                        // shutter speed (1/125)
-
-    0,                         // exp_comp
-
-    // aperture_string and shutter_speed_string are initialized in
-    // initialize_global_meter_state();
-};
+meter_state_t global_meter_state;
 
 void write_meter_state(const meter_state_t *ms)
 {
