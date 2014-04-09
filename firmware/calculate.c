81b028825c32c2cd1224254534ad1c4cc6c1a8a5
Alex Drummond <a.d.drummond@gmail.com>
2014-04-09 18:29:10 -0400

    Fix sign bug in temperature compensation code in calculate.c

--- firmware/calculate.c
+++ firmware/calculate.c
@@ -79,15 +79,15 @@ uint8_t get_ev100_at_temperature_voltage(uint8_t temperature, uint8_t voltage, g
 #endif
 
     // Compensate for effect of ambient temperature.
-    uint8_t adj = TEMP_EV_ADJUST[temperature >> 2];
+    int8_t adj = TEMP_EV_ADJUST[temperature >> 2];
     uint8_t withcomp = r + adj;
     // Check for overflow
-    if (adj < 0 && withcomp > r)
+        if (adj < 0 && withcomp > r)
         r = 0;
     else if (adj > 0 && withcomp < r)
         r = 255;
     else
-        r = withcomp;
+    r = withcomp;
 
     return r;
 
