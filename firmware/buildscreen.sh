111568d9604e4746dafdf7bfbf4cb8c4d50b3f0d
Alex Drummond <a.d.drummond@gmail.com>
2014-04-11 17:22:29 -0400

    Switch I2C libraries.

--- firmware/buildscreen.sh
+++ firmware/buildscreen.sh
@@ -4,7 +4,7 @@ VUSB=./vusb-20121206/usbdrv/
 CFLAGS="-I ./ -I./vusb-20121206/usbdrv/ -Wall -Wpadded -O2 -fno-move-loop-invariants -fno-tree-scev-cprop -fno-inline-small-functions -DF_CPU=16500000L -mmcu=attiny85"
 
 avr-gcc $CFLAGS -c screentest.c -o screentest.out
-avr-gcc $CFLAGS -c usitwislave.c -o usitwislave.out
+avr-gcc $CFLAGS -c i2cmaster.S -o i2cmaster.out
 avr-gcc $CFLAG usitwislave.out screentest.out -o screentestmain.out
 avr-objcopy -O ihex -R .eeprom screentestmain.out screentest.hex
 cp screentest.hex /tmp
--- firmware/screentest.c
+++ firmware/screentest.c
@@ -3,21 +3,60 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#include <usitwislave.h>
+#include <i2cmaster.h>
 
 // PB 2: RST
 // PB 1: Clck
 // PB 4: Data
 
+/* static void init_display()
+{
+    ssd1306_command(SSD1306_DISPLAYOFF);                    // 0xAE
+    ssd1306_command(SSD1306_SETDISPLAYCLOCKDIV);            // 0xD5
+    ssd1306_command(0x80);                                  // the suggested ratio 0x80
+    ssd1306_command(SSD1306_SETMULTIPLEX);                  // 0xA8
+    ssd1306_command(0x3F);
+    ssd1306_command(SSD1306_SETDISPLAYOFFSET);              // 0xD3
+    ssd1306_command(0x0);                                   // no offset
+    ssd1306_command(SSD1306_SETSTARTLINE | 0x0);            // line #0
+    ssd1306_command(SSD1306_CHARGEPUMP);                    // 0x8D
+    if (vccstate == SSD1306_EXTERNALVCC) 
+      { ssd1306_command(0x10); }
+    else 
+      { ssd1306_command(0x14); }
+    ssd1306_command(SSD1306_MEMORYMODE);                    // 0x20
+    ssd1306_command(0x00);                                  // 0x0 act like ks0108
+    ssd1306_command(SSD1306_SEGREMAP | 0x1);
+    ssd1306_command(SSD1306_COMSCANDEC);
+    ssd1306_command(SSD1306_SETCOMPINS);                    // 0xDA
+    ssd1306_command(0x12);
+    ssd1306_command(SSD1306_SETCONTRAST);                   // 0x81
+    if (vccstate == SSD1306_EXTERNALVCC) 
+      { ssd1306_command(0x9F); }
+    else 
+      { ssd1306_command(0xCF); }
+    ssd1306_command(SSD1306_SETPRECHARGE);                  // 0xd9
+    if (vccstate == SSD1306_EXTERNALVCC) 
+      { ssd1306_command(0x22); }
+    else 
+      { ssd1306_command(0xF1); }
+    ssd1306_command(SSD1306_SETVCOMDETECT);                 // 0xDB
+    ssd1306_command(0x40);
+    ssd1306_command(SSD1306_DISPLAYALLON_RESUME);           // 0xA4
+    ssd1306_command(SSD1306_NORMALDISPLAY);                 // 0xA6
+}*/
+
 int main()
 {
-    DDRB = 0b100;
-    PORTB |= 0b100;
+    DDRB = 0b1000;
+    PORTB |= 0b1000;
     _delay_ms(1);
-    PORTB &= ~0b100;
+    PORTB &= ~0b1000;
     _delay_ms(10);
-    PORTB |= 0b100;
+    PORTB |= 0b1000;
 
-    for (;;)
-        _delay_ms(50);
+    i2c_init();
+    
+
+    usi_twi_slave(0b0111100, 0, data_callback, idle_callback);
 }
