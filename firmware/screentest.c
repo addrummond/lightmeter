70d045526b3beb8c350f31e942392971c5cab587
Alex Drummond <a.d.drummond@gmail.com>
2014-04-14 01:01:09 -0400

    Non-page-aligned small digits now working.

--- firmware/screentest.c
+++ firmware/screentest.c
@@ -27,19 +27,19 @@ static void test_display()
         display_write_page_array(out, 12, 3, x, (y+41) >> 3);
         }*/
 
-    uint8_t out2[8];
+    uint8_t out2[16];
     memset(out2, 0, sizeof(out2));
-    display_bwrite_8x8_char(CHAR_8PX_0, out2, 1, 0);
-    display_write_page_array(out2, 8, 1, 20, 4);
+    display_bwrite_8x8_char(CHAR_8PX_0, out2, 2, 0);
+    display_write_page_array(out2, 8, 2, 20, 4);
     memset(out2, 0, sizeof(out2));
-    display_bwrite_8x8_char(CHAR_8PX_1, out2, 1, 0);
-    display_write_page_array(out2, 8, 1, 28, 4);
+    display_bwrite_8x8_char(CHAR_8PX_1, out2, 2, 1);
+    display_write_page_array(out2, 8, 2, 28, 4);
     memset(out2, 0, sizeof(out2));
-    display_bwrite_8x8_char(CHAR_8PX_2, out2, 1, 0);
-    display_write_page_array(out2, 8, 1, 36, 4);
+    display_bwrite_8x8_char(CHAR_8PX_2, out2, 2, 2);
+    display_write_page_array(out2, 8, 2, 36, 4);
     memset(out2, 0, sizeof(out2));
-    display_bwrite_8x8_char(CHAR_8PX_3, out2, 1, 0);
-    display_write_page_array(out2, 8, 1, 44, 4);
+    display_bwrite_8x8_char(CHAR_8PX_3, out2, 2, 3);
+    display_write_page_array(out2, 8, 2, 44, 4);
 
     for (;;);
 }
