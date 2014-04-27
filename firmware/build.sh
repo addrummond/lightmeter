3d477fb25856a45b0b2647b87d0f197552b7ec94
Alex Drummond <a.d.drummond@gmail.com>
2014-04-27 16:26:26 -0400

    Generate intermediate files in build.sh

--- firmware/build.sh
+++ firmware/build.sh
@@ -8,16 +8,18 @@ cd bitmaps
 python process_bitmaps.py output
 cd ../
 
-avr-gcc $CFLAGS -o tables.out -c tables.c
-avr-gcc $CFLAGS -o divmulutils.out -c divmulutils.c
-avr-gcc $CFLAGS -o calculate.out -c calculate.c
-avr-gcc $CFLAGS -o state.out -c state.c
-avr-gcc $CFLAGS -o bcd.out -c bcd.c
-avr-gcc $CFLAGS -o exposure.out -c exposure.c
-avr-gcc $CFLAGS -o display.out -c display.c
-avr-gcc $CFLAGS -o ui.out -c ui.c
-avr-gcc $CFLAGS -o bitmaps.out -c bitmaps/bitmaps.c
-avr-gcc $CFLAGS -o lightmeter.out -c lightmeter.c
+ST=-save-temps
+
+avr-gcc $ST $CFLAGS -o tables.out -c tables.c
+avr-gcc $ST $CFLAGS -o divmulutils.out -c divmulutils.c
+avr-gcc $ST $CFLAGS -o calculate.out -c calculate.c
+avr-gcc $ST $CFLAGS -o state.out -c state.c
+avr-gcc $ST $CFLAGS -o bcd.out -c bcd.c
+avr-gcc $ST $CFLAGS -o exposure.out -c exposure.c
+avr-gcc $ST $CFLAGS -o display.out -c display.c
+avr-gcc $ST $CFLAGS -o ui.out -c ui.c
+avr-gcc $ST $CFLAGS -o bitmaps.out -c bitmaps/bitmaps.c
+avr-gcc $ST $CFLAGS -o lightmeter.out -c lightmeter.c
 avr-gcc $CFLAGS $LINKFLAGS calculate.out tables.out divmulutils.out state.out bcd.out exposure.out display.out ui.out bitmaps.out lightmeter.out -o main.out
 avr-objcopy -O ihex -R .eeprom main.out lightmeter.hex
 cp lightmeter.hex /tmp
