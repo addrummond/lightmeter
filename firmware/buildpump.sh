955ee87ed1bb1f5117cc05145ed56d40d4e84721
Alex Drummond <a.d.drummond@gmail.com>
2014-04-28 01:24:54 -0400

    Remove some currently-irrelevant test code.

--- firmware/buildpump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CFLAGS=`cat CFLAGS`
-
-avr-gcc $CFLAGS -c chargepumptest.c -o chargepumptest.out
-avr-objcopy -O ihex -R .eeprom chargepumptest.out chargepumptest.hex
-cp chargepumptest.hex /tmp
