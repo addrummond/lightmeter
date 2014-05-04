675c3def2d0058d03c04d67177355847cd956224
Alex Drummond <a.d.drummond@gmail.com>
2014-05-04 14:34:08 -0400

    Remove divmulutils from buildexposuretest.sh

--- firmware/buildexposuretest.sh
+++ firmware/buildexposuretest.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 gcc -I./ -c bcd.c -o bcd.testout
-gcc -I./ -c divmulutils.c -o divmulutils.out
 gcc -I./ -DTEST -c exposure.c -o exposure.testout
 gcc -I./ -DTEST -c tables.c -o tables.testout
-gcc divmulutils.out bcd.testout exposure.testout tables.testout -o testexposure
+gcc bcd.testout exposure.testout tables.testout -o testexposure
