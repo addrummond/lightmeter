a9f580b090be3bb4a15ab3dbcf11e98e14302c06
Alex Drummond <a.d.drummond@gmail.com>
2014-05-18 12:50:13 -0400

    Unset CLKDIV fuse

--- firmware/programfuses.sh
+++ firmware/programfuses.sh
@@ -2,4 +2,4 @@
 
 avrdude -B 3 -c avrispmkII -p attiny1634 -U "efuse:w:0x1F:m"
 avrdude -B 3 -c avrispmkII -p attiny1634 -U "hfuse:w:0xDF:m"
-avrdude -B 3 -c avrispmkII -p attiny1634 -U "lfuse:w:0x62:m"
+avrdude -B 3 -c avrispmkII -p attiny1634 -U "lfuse:w:0xE2:m"
