5cfe09c5e43411b95d4c9cfab16ad6bf01e3fe71
Alex Drummond <a.d.drummond@gmail.com>
2014-05-18 18:49:21 -0400

    Modify program.sh to avoid unnecessary number of invocations of avrdude.

--- firmware/program.sh
+++ firmware/program.sh
@@ -3,11 +3,5 @@
 HEXFILE=$1
 EEPROMFILE=$2
 
-if [ "$1" != "X" ]; then
-    echo "Programming flash file $HEXFILE..."
-    avrdude -p attiny1634 -c avrispmkII -P usb -B 3 -Uflash:w:$HEXFILE
-fi
-if [ -n "$EEPROMFILE" ]; then
-    echo "Programming eeprom file $EEPROMFILE..."
-    avrdude -p attiny1634 -c avrispmkII -P usb -B 3 -Ueeprom:w:$EEPROMFILE
-fi
+echo "Programming flash file $HEXFILE and eeprom file $EEPROMFILE..."
+avrdude -p attiny1634 -c avrispmkII -P usb -B 3 -Uflash:w:$HEXFILE -Ueeprom:w:$EEPROMFILE
