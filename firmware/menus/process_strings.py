6d45db607a835758bae634ce1bac01a491dd7359
Alex Drummond <a.d.drummond@gmail.com>
2014-05-24 14:04:05 -0400

    Some more work on process_strings.py

--- firmware/menus/process_strings.py
+++ firmware/menus/process_strings.py
@@ -1,6 +1,53 @@
 import sys
 import re
 
+#
+# Strings for menus are stored as arrays of 1-byte indices into a string table.
+# Each string in the table consists of a zero-terminated sequence of compacted
+# 6-bit characters, where each character is an offset in the the relevant table
+# of characters.
+#
+
+def map_char_to_12px_name(c):
+    if re.match(r"[A-Za-z]", c):
+        return 'CHAR_12PX_' + c.upper()
+    elif c == '/':
+        return 'CHAR_12PX_SLASH'
+    elif c == ' ':
+        return '63'
+    else:
+        sys.stderr.write("Could not map char '%s' to 12px name\n" % c)
+        sys.exit(1)
+
+def compact_string(string):
+    out = ""
+    n_bytes = (len(string)*8/6) + 1
+    bytes = [[] for x in xrange(n_bytes)]
+    for i in xrange(len(string)+1):
+        byte_i = i*6/8
+        bit_offset = i*6%8
+        c = None
+        if i == len(string):
+            c = '0'
+        else:
+            c = map_char_to_12px_name(string[i]) + '_O'
+        val = '(' + c + ' << ' + str(bit_offset) + ')'
+        bytes[byte_i].append(val)
+        if bit_offset > 2:
+            val = '(' + c + ' >> ' + str(6 - (8 - bit_offset)) + ')'
+            bytes[byte_i+1].append(val)
+    while len(bytes[-1]) == 0:
+        bytes.pop()
+    for byte in bytes:
+        out += '|'.join(byte) + ','
+    return out
+
+def output_strings_table(f, strings, defines):
+    f.write('const uint8_t MENU_STRING_TABLE[] PROGMEM = {\n')
+    for v in defines.itervalues():
+        f.write(compact_string(v))
+    f.write('\n};\n')
+
 blank_or_comment_regex = re.compile(r"^\s*(?:#.*)?$")
 line_regex = re.compile(r"^\s*(\w*)\s*(?:@(\w+))?\s*\{((?:(?:[^}\\])|(?:\\.))*)\}\s*(?:#.*)?$")
 text_regex = re.compile(r"((?:[^$]+)|(?:\$\w*))")
@@ -39,8 +86,7 @@ def process(contents):
 
         line_number += 1
 
-    print strings
-    print defines
+    output_strings_table(sys.stdout, strings, defines)
 
 if __name__ == '__main__':
     fname = sys.argv[1]
