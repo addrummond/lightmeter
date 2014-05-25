6b7d7f90029bc6638c2362a50139f45615f26814
Alex Drummond <a.d.drummond@gmail.com>
2014-05-24 20:09:02 -0400

    Minor.

--- firmware/menus/menu_strings.h
+++ firmware/menus/menu_strings.h
@@ -7,6 +7,6 @@
 
 void menu_string_decode_(bool get_long, const uint8_t *menu_string, uint8_t *dest);
 #define menu_string_decode_short(x,y) menu_string_decode_(false, x, y)
-#define menu_string_decode_long(x, y) menu_string_decode_(true, x, y)
+#define menu_string_decode_long(x,y) menu_string_decode_(true, x, y)
 
 #endif
