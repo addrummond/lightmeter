172a0d6422974eedeaa2123b9c253d261e88cd62
Alex Drummond <a.d.drummond@gmail.com>
2014-11-02 22:29:07 -0500

    Edits to comment in deviceconfig.h

--- firmware/deviceconfig.h
+++ firmware/deviceconfig.h
@@ -14,11 +14,11 @@
 //  could be re-used, but this seems to cause weird issues in practice, even
 //  when the programmer is disconnected.
 //
-//        Display data   |    ADC5/PB0 1---20 [MOSI]    | **********
-//        Display DC     |    ADC4/PA7 2---19 [MISO]    | **********
-//   Temp sensor         |    ADC3/PA6 3---18 PB3/ADC8  |
+//          Display data |    ADC5/PB0 1---20 [MOSI]    | **********
+//            Display DC |    ADC4/PA7 2---19 [MISO]    | **********
+//           Temp sensor |    ADC3/PA6 3---18 PB3/ADC8  |
 //     Serial debug port |    ADC2/PA5 4---17 PC0/ADC9  | Op amp output
-//     Shift register A  |    ADC1/PA4 5---16 [USCK]    | **********
+//      Shift register A |    ADC1/PA4 5---16 [USCK]    | **********
 //          Pushbutton 1 |    ADC0/PA3 6---15 PC2/ADC11 | Pushbutton 3
 //          Pushbutton 2 |         PA2 7---14 [RESET]   | **********
 //  Shift register clock |         PA1 8---13 PC4       | Display CS
