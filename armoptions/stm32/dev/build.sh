74c3d52cddc8e399c78a830296c94084d801d1f4
Alex Drummond <a.d.drummond@gmail.com>
2015-01-03 12:53:01 +0000

    More work on arm migration.

--- armoptions/stm32/dev/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CC=~/opt/gcc-arm-none-eabi-4_9-2014q4/bin/arm-none-eabi-gcc
-CFLAGS="-DSTM32F030 -nostdlib -I./include"
-$CC $CFLAGS -c test.c -o test.o
-$CC $CFLAGS -c startup_stm32f030.s -o startup_stm32f030.o
-$CC $CFLAGS -c system_stm32f0xx.c -o system_stm32f0xx.o
-$CC $CFLAGS -T STM32F030R8_FLASH.ld test.o startup_stm32f030.o system_stm32f0xx.o -o test.elf
-
--- firmware/armbuild.sh
+++ firmware/armbuild.sh
@@ -7,5 +7,16 @@ ST=-save-temps
 CC=arm-none-eabi-gcc
 CFLAGS="$ST $CFLAGS"
 
+OBJS=""
+
+# Arm STM stuff.
+$CC $CFLAGS -c ./stm/system_stm32f0xx.c -o ./stm/system_stm32f0xx.o
+OBJS="$OBJS ./stm/system_stm32f0xx.o"
+$CC $CFLAGS -c ./stm/startup_stm32f030.s -o ./stm/startup_stm32f030.o
+OBJS="$OBJS ./stm/startup_stm32f030.o"
+
 $CC $CFLAGS -c exposure.c -o exposure.o
+OBJS="$OBJS exposure.o"
 $CC $CFLAGS -c bcd.c -o bcd.o
+OBJS="$OBJS bcd.o"
+
