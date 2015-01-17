50f49315664c661440f51bddd640fcc5ccd43f2e
Alex Drummond <a.d.drummond@gmail.com>
2015-01-16 23:34:40 -0500

    Some more debug tweaking.

--- firmware/main.c
+++ firmware/main.c
@@ -20,30 +20,12 @@ int main()
     display_init();
     debugging_writec("Here2\n");
 
-    display_reset();
     debugging_writec("Here3\n");
+    display_clear();
+    debugging_writec("Here33\n");
     const uint8_t page_array[] = { 0xFF, 0xFF };
     display_write_page_array(page_array, 2, 1, 20, 1);
     debugging_writec("Here4\n");
 
     for (;;);
-
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-
-    GPIO_InitTypeDef ledgpio;
-    ledgpio.GPIO_Pin = GPIO_Pin_9;
-    ledgpio.GPIO_Mode = GPIO_Mode_OUT;
-    ledgpio.GPIO_Speed = GPIO_Speed_Level_1;
-    ledgpio.GPIO_OType = GPIO_OType_PP;
-    ledgpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOC, &ledgpio);
-
-    BitAction value;
-    for (value = 1;; value = !value) {
-        GPIO_WriteBit(GPIOC, GPIO_Pin_9, value);
-        uint32_t c;
-        for (c = 0; c < 1000000; ++c);
-    }
-
-    return 0;
 }
