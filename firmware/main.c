b490439af9cbdd2dc8146ea5b2888925706a2467
Alex Drummond <a.d.drummond@gmail.com>
2015-01-17 22:09:53 -0500

    Improve test code in main.c

--- firmware/main.c
+++ firmware/main.c
@@ -4,6 +4,8 @@
 
 #include <i2c.h>
 #include <display.h>
+#include <ui.h>
+#include <state.h>
 #include <debugging.h>
 
 int main()
@@ -12,18 +14,42 @@ int main()
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 
-
     debugging_writec("Hello World!\n");
 
     i2c_init();
     display_init();
 
-    // Temporary test code: draw's rectangle on screen.
+    //
+    // Temporary test code.
+    //
+    // Sets bogus UI state to show some text.
+    //
+
     display_clear();
-    const uint8_t page_array[] = { 0xFF, 0xFF, 0xFF, 0xFF };
-    unsigned x;
-    for (x = 20; x < 40; ++x)
-        display_write_page_array(page_array, 2, 2, x, 1);
+
+#define gms global_meter_state
+#define tms global_transient_meter_state
+    gms.bcd_iso_digits[0] = 1;
+    gms.bcd_iso_digits[1] = 0;
+    gms.bcd_iso_digits[2] = 0;
+    gms.bcd_iso_length = 3;
+    ev_with_fracs_set_ev8(gms.stops_iso, 8*3);
+    gms.priority = SHUTTER_PRIORITY;
+    gms.exp_comp = 0;
+    gms.ui_mode = UI_MODE_READING;
+    gms.meter_mode = METER_MODE_INCIDENT;
+    gms.precision_mode = PRECISION_MODE_TENTH;
+    ev_with_fracs_set_ev8(gms.priority_aperture, 8*8);
+    ev_with_fracs_set_ev8(gms.priority_shutter_speed, 8*8);
+
+    ev_with_fracs_set_ev8(tms.last_ev_with_fracs, 8*8);
+    ev_with_fracs_set_ev8(tms.aperture, 8*8);
+    ev_with_fracs_set_ev8(tms.shutter_speed, 8*8);
+    tms.exposure_ready = true;
+    tms.op_amp_resistor_stage = 2;
+#undef gms
+#undef tms
+    ui_show_interface();
 
     for (;;);
 }
