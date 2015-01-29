f3909db146e033505385ef489043c866e100233a
Alex Drummond <a.d.drummond@gmail.com>
2015-01-28 22:08:39 -0500

    More work on case 3d model.

--- case/case.openjscad.js
+++ case/case.openjscad.js
@@ -1,16 +1,9 @@
-// title: OpenJSCAD.org Logo
-// author: Rene K. Mueller
-// license: Creative Commons CC BY
-// URL: http://openjscad.org/#examples/logo.jscad
-// revision: 0.003
-// tags: Logo,Intersection,Sphere,Cube
-
-var THICK_MARGIN = 0.25
+var THICK_MARGIN = 0.25;
 var THICK =
-    1.75 + // Screen (buttons are slimmer)
-    2.75 + // USB connector (highest thing on top layer)
-    0.6 +  // Thinnest PCB we can have made cheaply.
-    THICK_MARGIN;
+1.75 + // Screen (buttons are slimmer)
+2.75 + // USB connector (highest thing on top layer)
+0.6 +  // Thinnest PCB we can have made cheaply.
+THICK_MARGIN;
 
 var PCB_WIDTH = 26.7;
 var PCB_HEIGHT = 33.0708;
@@ -26,21 +19,24 @@ var PCB_LEDGE_WIDTH = 2.8;
 var PCB_THICK = 0.6;
 var SCREEN_THICK = 1.75;
 var LEDGE_BOTTOM = PCB_THICK +
-                   SCREEN_THICK;
+SCREEN_THICK;
 
 var SENSOR_POSITIONS = [
-    [3.81, PCB_HEIGHT-4.4958],
-    [6.35, PCB_HEIGHT-4.4958],
-    [15.24, PCB_HEIGHT-4.4958],
-    [17.78, PCB_HEIGHT-4.4958]
+[3.81, PCB_HEIGHT-4.4958],
+[6.35, PCB_HEIGHT-4.4958],
+[15.24, PCB_HEIGHT-4.4958],
+[17.78, PCB_HEIGHT-4.4958]
 ];
 
 var SENSOR_WIDTH = 1.6;
 var SENSOR_HEIGHT = SENSOR_WIDTH;
+var SENSOR_HOLE_EXTRA = 2;
 
 var SCREEN_FROM_TOP = 2;
 var SCREEN_WIDTH = 26.7;
 var SCREEN_HEIGHT = 19.26;
+
+var TOTAL_WIDTH = WIDTH + THICK + (CASE_THICK*2);
 
 function make_box(w,h,t) {
     var obox2d = hull(
@@ -56,25 +52,27 @@ function main() {
     var ibox = make_box(WIDTH, HEIGHT, THICK).translate([CASE_THICK, CASE_THICK, CASE_THICK]);
     var ledge = cube({
         size: [
-            PCB_LEDGE_WIDTH+THICK-0.2,
-            HEIGHT,
-            1.0
+        PCB_LEDGE_WIDTH+THICK-0.2,
+        HEIGHT,
+        1.0
         ]
     }).translate([0.2, -HEIGHT, THICK-LEDGE_BOTTOM]);
-
+    
     var box = obox.subtract(ibox).union(ledge);
+    var h = [ ];
     for (var i = 0; i < SENSOR_POSITIONS.length; ++i) {
         var x = SENSOR_POSITIONS[i][0];
         var y = SENSOR_POSITIONS[i][1];
-
-        box = box.subtract(
-            cube({size: [ SENSOR_WIDTH, SENSOR_HEIGHT, THICK ]}).translate([x+THICK, -y, 0])
-        );
+        h.push(square([SENSOR_WIDTH+SENSOR_HOLE_EXTRA, SENSOR_HEIGHT+SENSOR_HOLE_EXTRA])
+        .translate([x+THICK/2+CASE_THICK,-y]));
     }
+
+    var sensor_hole = linear_extrude({ height: CASE_THICK }, chain_hull(h));
+    box = box.subtract(sensor_hole);
 
     box = box.subtract(cube({
         size: [ SCREEN_WIDTH, SCREEN_HEIGHT, CASE_THICK ]
-    }).translate([  , -(HEIGHT-SCREEN_FROM_TOP), THICK+CASE_THICK ]));
+    }).translate([THICK/2+CASE_THICK, -(HEIGHT-SCREEN_FROM_TOP), THICK+CASE_THICK ]));
 
     return box;
 }
