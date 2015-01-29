a7e50c461cca747c041089bb46d9156aa8d3b4d0
Alex Drummond <a.d.drummond@gmail.com>
2015-01-29 15:55:11 -0500

    More work on case design.

--- case/case.openjscad.js
+++ case/case.openjscad.js
@@ -1,14 +1,16 @@
 var THICK_MARGIN = 0.25;
 var THICK =
-1.75 + // Screen (buttons are slimmer)
-2.75 + // USB connector (highest thing on top layer)
-0.6 +  // Thinnest PCB we can have made cheaply.
-THICK_MARGIN;
+    1.75 + // Screen (buttons are slimmer)
+    2.75 + // USB connector (highest thing on top layer)
+    0.6 +  // Thinnest PCB we can have made cheaply.
+    THICK_MARGIN;
 
 var PCB_WIDTH = 26.7;
-var PCB_HEIGHT = 33.0708;
-
+var PCB_HEIGHT = 38.1508;
 var PCB_HORIZ_MARGIN = 0.5;
+
+var BATTERY_HEIGHT = 35;
+var BATTERY_THICK = 5;
 
 var WIDTH = PCB_WIDTH + PCB_HORIZ_MARGIN;
 var HEIGHT = PCB_HEIGHT + PCB_HORIZ_MARGIN;
@@ -18,14 +20,13 @@ var CASE_THICK = 0.5;
 var PCB_LEDGE_WIDTH = 2.8;
 var PCB_THICK = 0.6;
 var SCREEN_THICK = 1.75;
-var LEDGE_BOTTOM = PCB_THICK +
-SCREEN_THICK;
+var LEDGE_BOTTOM = PCB_THICK + SCREEN_THICK;
 
 var SENSOR_POSITIONS = [
-[3.81, PCB_HEIGHT-4.4958],
-[6.35, PCB_HEIGHT-4.4958],
-[15.24, PCB_HEIGHT-4.4958],
-[17.78, PCB_HEIGHT-4.4958]
+    [3.81, 4.4958],
+    [6.35, 4.4958],
+    [15.24, 4.4958],
+    [17.78, 4.4958]
 ];
 
 var SENSOR_WIDTH = 1.6;
@@ -38,22 +39,38 @@ var SCREEN_HEIGHT = 19.26;
 
 var TOTAL_WIDTH = WIDTH + THICK + (CASE_THICK*2);
 
-var BIG_BUTTON_WIDTH = 13;
+var BIG_BUTTON_WIDTH = 11;
 var BIG_BUTTON_HEIGHT = 8;
 var BIG_BUTTON_THICK = 1;
 var BIG_BUTTON_NOB_HEIGHT = 1;
 var BIG_BUTTON_NOB_RAD = 1.5;
-var BIG_BUTTON_FROM_BOTTOM = 2;
+var BIG_BUTTON_CENTER_FROM_BOTTOM = 2.667;
+var BUTTON_CENTER_DIV = 3;
 
 var BUTTON_HOLE_MARGIN = 0.1;
 
-function make_box(w,h,t) {
-    var obox2d = hull(
-        circle(t/2),
-        circle(t/2).translate([w,0,0])
-    );
-    var obox = linear_extrude({ height: h }, obox2d);
-    return obox.rotateX(90);
+function make_box(w,h,t,case_thick) {
+    var rect = cube({size: [w,h,t]});
+    rect = expand(case_thick, 12, rect);
+    return rect;
+}
+
+function make_hollow_box(w, h, t, case_thick, ledge_height) {
+    var obox = make_box(w, h, t, case_thick);
+    var ibox = cube({size: [w,h+case_thick,t]});
+
+    var box = obox.subtract(ibox);
+    if (ledge_height) {
+        var ledge = cube({
+            size: [
+            PCB_LEDGE_WIDTH,
+            ledge_height,
+            1.0
+            ]
+        }).translate([0.2, 0, t-LEDGE_BOTTOM]);
+        box = box.union(ledge);
+    }
+    return box;
 }
 
 function make_big_button(pad){
@@ -65,38 +82,29 @@ function make_big_button(pad){
 }
 
 function main() {
-    var obox = make_box(WIDTH, HEIGHT+CASE_THICK, THICK+CASE_THICK*2);
-    var ibox = make_box(WIDTH, HEIGHT, THICK).translate([CASE_THICK, CASE_THICK, CASE_THICK]);
-    var ledge = cube({
-        size: [
-        PCB_LEDGE_WIDTH+THICK-0.2,
-        HEIGHT,
-        1.0
-        ]
-    }).translate([0.2, -HEIGHT, THICK-LEDGE_BOTTOM]);
-
-    var box = obox.subtract(ibox).union(ledge);
+    var box = make_hollow_box(WIDTH, HEIGHT+BATTERY_HEIGHT, THICK, CASE_THICK, HEIGHT);
     var h = [ ];
     for (var i = 0; i < SENSOR_POSITIONS.length; ++i) {
         var x = SENSOR_POSITIONS[i][0];
         var y = SENSOR_POSITIONS[i][1];
         h.push(square([SENSOR_WIDTH+SENSOR_HOLE_EXTRA, SENSOR_HEIGHT+SENSOR_HOLE_EXTRA])
-        .translate([x+THICK/2+CASE_THICK,-y]));
+        .translate([x+THICK/2+CASE_THICK,y]));
     }
 
     var sensor_hole = linear_extrude({ height: CASE_THICK }, chain_hull(h));
+    sensor_hole = sensor_hole.translate(-CASE_THICK);
     box = box.subtract(sensor_hole);
 
     box = box.subtract(cube({
         size: [ SCREEN_WIDTH, SCREEN_HEIGHT, CASE_THICK ]
-    }).translate([THICK/2+CASE_THICK, -(HEIGHT-SCREEN_FROM_TOP), THICK+CASE_THICK ]));
+    }).translate([0, SCREEN_FROM_TOP, THICK ]));
 
     var bigbuthole = make_big_button(BUTTON_HOLE_MARGIN)
-    .translate([(TOTAL_WIDTH-BIG_BUTTON_WIDTH-BUTTON_HOLE_MARGIN*2)/2, -BIG_BUTTON_FROM_BOTTOM-BIG_BUTTON_HEIGHT-BUTTON_HOLE_MARGIN, THICK]);
+    .translate([(WIDTH-BIG_BUTTON_WIDTH-BUTTON_HOLE_MARGIN*2)/2, BATTERY_HEIGHT+BIG_BUTTON_CENTER_FROM_BOTTOM-BIG_BUTTON_HEIGHT/BUTTON_CENTER_DIV-BUTTON_HOLE_MARGIN, THICK-BIG_BUTTON_THICK+CASE_THICK]);
     box = box.subtract(bigbuthole);
 
     var bigbut = make_big_button(0)
-    .translate([(TOTAL_WIDTH-BIG_BUTTON_WIDTH)/2, -BIG_BUTTON_FROM_BOTTOM-BIG_BUTTON_HEIGHT, THICK]);
+    .translate([(WIDTH-BIG_BUTTON_WIDTH)/2, BATTERY_HEIGHT+BIG_BUTTON_CENTER_FROM_BOTTOM-BIG_BUTTON_HEIGHT/BUTTON_CENTER_DIV, THICK-BIG_BUTTON_THICK+CASE_THICK]);
 
     return box.union(bigbut);
 }
