c3e8327c279c7c53330ffcbc8349181cf08befc0
Alex Drummond <a.d.drummond@gmail.com>
2015-03-15 20:04:22 -0400

    Add some more functionality to rawsamplestowav.py

--- firmware/fft/rawsamplestowav.py
+++ firmware/fft/rawsamplestowav.py
@@ -5,7 +5,7 @@ import re
 
 def samples_to_wave(samples, of):
     w = wave.open(of, 'w')
-    w.setparams((1, 1, 44000, len(samples), 'NONE', 'not compressed'))
+    w.setparams((1, 1, 44100, len(samples), 'NONE', 'not compressed'))
     w.writeframes(bytearray((s+128 if s < 128 else -((256-s)-128) for s in samples)))
     w.close()
 
@@ -33,6 +33,30 @@ def get_samples_from_text(s):
         i += 1
     return samples
 
+def get_samples_from_webapp(s):
+    lines = re.split(r"\r?\n", s)
+    samples = bytearray(len(lines))
+    i = 0
+    for l in lines:
+        if re.match(r"^\s*$", l):
+            continue
+
+        m = re.match(r"\s*([^\s]+)\s*$", l)
+        if not m:
+            raise Exception("Bad file format [1]")
+        v = None
+        try:
+            v = float(m.group(1))
+        except ValueError:
+            raise Exception("Bad file format [2]")
+
+        if v < -1.0 or v > 1.0:
+            raise Exception("Bad file format [3]")
+
+        samples[i] = int(round((v+1.0)*128.0))
+        i += 1
+    return samples
+
 if __name__ == '__main__':
     inputfile = sys.argv[1]
     outputfile = sys.argv[2]
@@ -40,7 +64,11 @@ if __name__ == '__main__':
 
     f = open(inputfile)
     c = f.read()
-    samples = get_samples_from_text(c) * times_to_repeat
+    samples = None
+    if inputfile.endswith(".web"):
+        samples = get_samples_from_webapp(c)
+    else:
+        samples = get_samples_from_text(c) * times_to_repeat
     if outputfile.endswith(".wav"):
         samples_to_wave(samples, outputfile)
     else:
