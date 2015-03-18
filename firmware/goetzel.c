6d40c406042367eb4600df6ae9cc86e58d7be101
Alex Drummond <a.d.drummond@gmail.com>
2015-03-18 03:29:27 -0400

    More fiddling with goetzel.c

--- firmware/goetzel.c
+++ firmware/goetzel.c
@@ -49,23 +49,20 @@ uint32_t goetzel(goetzel_state_t *st, int32_t sample)
     st->prev2 = st->prev1;
     st->prev1 = s;
     ++(st->n);
-    uint32_t t1 = MUL(st->prev2,st->prev2),
-             t2 = MUL(st->prev1,st->prev1),
-             t3 = MUL(st->coeff,MUL(st->prev1,st->prev2));
-    uint32_t p = t1 + t2 - t3;
-    st->power += MUL(p,p)   ;
+    int32_t p = MUL(st->prev2,st->prev2) + MUL(st->prev1,st->prev1) - MUL(MUL(st->coeff,st->prev1),st->prev2);
+    st->power += MUL(sample,sample);
     if (st->power == 0)
         st->power = 1;
-    return DIV(p,DIV(st->power,st->n));
+    return DIV(DIV(p,st->power),st->n);
 }
 
 #ifdef TEST
 int main()
 {
     const float SAMPLE_FREQ = 40000;
-    const float SIG_FREQ = 5000;
+    const float SIG_FREQ = 1000;
     const float NFREQ = SIG_FREQ/SAMPLE_FREQ;
-    const int32_t COEFF = FLOAT_TO_FIX(2*cos(2*M_PI*NFREQ*2));
+    const int32_t COEFF = FLOAT_TO_FIX(2*cos(2*M_PI*NFREQ));
 
     goetzel_state_t gs;
     goetzel_state_init(&gs, COEFF);
@@ -74,10 +71,10 @@ int main()
     for (i = 0; i < 512; ++i) {
         float t = i/SAMPLE_FREQ;
         float sample_value = 0.45*(sin(2.0*M_PI*SIG_FREQ*t));
-        int32_t sample12 = sample_value*(4096/2.0);
+        int32_t sample12 = (int32_t)(sample_value*(4096/2.0));
         uint32_t p = goetzel(&gs, sample12);
         //printf("Power at t=%f, sample=%f: %f\n", t, sample_value, sqrt((float)(p/(4096.0/2))));
-        printf("%f,%f,%f\n", t, sample_value, FIX_TO_FLOAT(p));
+        printf("%f,%f,%i,%i\n", t, sample_value, sample12, p);
     }
 }
 #endif
