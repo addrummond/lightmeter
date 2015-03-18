c054bcafa6def051cbc9b11c53e6f1d9088104aa
Alex Drummond <a.d.drummond@gmail.com>
2015-03-18 17:38:32 -0400

    Update coefficients in piezo.h

--- firmware/piezo.h
+++ firmware/piezo.h
@@ -7,25 +7,25 @@
 
 #define PIEZO_MIC_BUFFER_N_SAMPLES       256
 
+//
+// We're sampling at 239.5 clock cycles, running at 48MHz.
+// Thus, our sample rate is 200418Hz.
+//
+// The coefficient for the Goetzel algorithm is calculated using the following
+// formula:
+//
+//     coeff = 2cos(2*pi*(f/200418))
+//
+
 #define PIEZO_HFSDP_A_MODE_MASTER_CLOCK_HZ    5000.0
-#define PIEZO_HFSDP_A_MODE_MASTER_CLOCK_COEFF GOETZEL_FLOAT_TO_FIX(1.314337413286371)
+#define PIEZO_HFSDP_A_MODE_MASTER_CLOCK_COEFF GOETZEL_FLOAT_TO_FIX(1.9754790747099111)
 #define PIEZO_HFSDP_A_MODE_MASTER_DATA_HZ     6000.0
-#define PIEZO_HFSDP_A_MODE_MASTER_DATA_COEFF  GOETZEL_FLOAT_TO_FIX(1.0390727525380696)
+#define PIEZO_HFSDP_A_MODE_MASTER_DATA_COEFF  GOETZEL_FLOAT_TO_FIX(1.9647216815939208)
 
 #define PIEZO_HFSDP_A_MODE_MASTER_F1_HZ  5000.0
 #define PIEZO_HFSDP_A_MODE_MASTER_F2_HZ  5500.0
 #define PIEZO_HFSDP_A_MODE_SLAVE_F1_HZ   7000.0
 #define PIEZO_HFSDP_A_MODE_SLAVE_F2_HZ   11000.0
-// Estimated by testing. Each element in the sequence outputted by the FFT
-// appears to correspond to about 217.4Hz when ADC samples at 239.5 clock
-// cycles (and processor is at 48MHz). From this we can deduce that our
-// current sample rate is about 36798Hz. (We'll want to bump this up to
-// detect ultrasound.)
-#define PIEZO_HZ_TO_FTF(hz)              (((unsigned)((hz)/217.4))+1)
-#define PIEZO_HFSDP_A_MODE_MASTER_F1_FTF PIEZO_HZ_TO_FTF(PIEZO_HFSDP_A_MODE_MASTER_F1_HZ)
-#define PIEZO_HFSDP_A_MODE_MASTER_F2_FTF PIEZO_HZ_TO_FTF(PIEZO_HFSDP_A_MODE_MASTER_F2_HZ)
-#define PIEZO_HFSDP_A_MODE_SLAVE_F1_FTF  PIEZO_HZ_TO_FTF(PIEZO_HFSDP_A_MODE_SLAVE_F1_HZ)
-#define PIEZO_HFSDP_A_MODE_SLAVE_F2_FTF  PIEZO_HZ_TO_FTF(PIEZO_HFSDP_A_MODE_SLAVE_F2_HZ)
 
 void piezo_mic_init(void);
 void piezo_mic_deinit(void);
