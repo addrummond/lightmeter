90327dea4507631e470926174d1779cc9fd02e30
Alex Drummond <a.d.drummond@gmail.com>
2015-03-18 13:39:25 -0400

    Remove fft lib.
    This runs too slowly to be of any practical use. Switching to Goetzel algorithm
    instead.

--- firmware/fft/buildffttest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -I./ fix_fft.c test_fix_fft.c -o testfft
--- firmware/fft/fix_fft.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIX_FFT_H
-#define FIX_FFT_H
-
-#include <stdint.h>
-
-int fix_fft(int8_t fr[], int8_t fi[], int m, int inverse);
-int fix_fftr(int8_t f[], int m, int inverse);
-
-#endif
