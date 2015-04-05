027609bb5d93aba7170b76812220a9fed63c361c
Alex Drummond <a.d.drummond@gmail.com>
2015-04-05 12:54:34 -0400

    Remove program.sh (hangover from old AVR code)

--- firmware/program.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-HEXFILE=$1
-EEPROMFILE=$2
-
-echo "Programming flash file $HEXFILE and eeprom file $EEPROMFILE..."
-avrdude -p attiny1634 -c avrispmkII -P usb -B 3 -Uflash:w:$HEXFILE -Ueeprom:w:$EEPROMFILE
