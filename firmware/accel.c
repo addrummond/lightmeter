ed8a02708cc13c5b6fc315abb8c933292c70129a
Alex Drummond <a.d.drummond@gmail.com>
2015-05-13 21:20:00 -0400

    Minor.

--- firmware/accel.c
+++ firmware/accel.c
@@ -8,12 +8,12 @@
 // Single byte write
 void accel_write_register(uint8_t reg, uint8_t byte)
 {
-    I2C_WAIT_ON_FLAG_NO_RESET(I2C_ISR_BUSY, "sbw (accel) 1");
+    I2C_WAIT_ON_FLAG_NO_RESET(I2C_ISR_BUSY, "accel_write_register 1");
     I2C_TransferHandling(I2C_I2C, ACCEL_I2C_ADDR, 2, I2C_AutoEnd_Mode, I2C_Generate_Start_Write);
-    I2C_WAIT_ON_FLAG_RESET(I2C_ISR_TXIS, "sbw (accel) 2");
+    I2C_WAIT_ON_FLAG_RESET(I2C_ISR_TXIS, "accel_write_register 2");
     I2C_SendData(I2C_I2C, reg);
     I2C_SendData(I2C_I2C, byte);
-    I2C_WAIT_ON_FLAG_RESET(I2C_ISR_STOPF, "sbw (accel) 3");
+    I2C_WAIT_ON_FLAG_RESET(I2C_ISR_STOPF, "accel_write_register 3");
 }
 
 uint8_t accel_read_register(uint8_t reg)
@@ -22,11 +22,11 @@ uint8_t accel_read_register(uint8_t reg)
     I2C_TransferHandling(I2C_I2C, ACCEL_I2C_ADDR, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
     I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_TXIS, "accel_read_reg 2");
     I2C_SendData(I2C_I2C, reg);
-    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_TC, "accel_read_rec 2.5");
+    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_TC, "accel_read_rec 3");
     I2C_TransferHandling(I2C_I2C, ACCEL_I2C_ADDR, 1, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
-    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_RXNE, "accel_read_reg 3");
+    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_RXNE, "accel_read_reg 4");
     uint8_t v = I2C_ReceiveData(I2C_I2C);
-    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_STOPF, "accel_read_reg 4");
+    I2C_WAIT_ON_FLAG_RESET_NORET(I2C_ISR_STOPF, "accel_read_reg 5");
     I2C_ClearFlag(I2C_I2C, I2C_ICR_STOPCF);
     return v;
 }
