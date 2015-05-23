4b0a738484b831cf55ae86d225d0daf6cf61cdac
Alex Drummond <a.d.drummond@gmail.com>
2015-05-23 19:25:02 -0400

    More work on partfinder.py

--- schematic/partfinder.py
+++ schematic/partfinder.py
@@ -8,34 +8,37 @@ import sys
 API_KEY = os.environ['OCTOPART_API_KEY']
 API_BASE_URL = """http://octopart.com/api/v3/"""
 
-def parse_value(v):
+def convert_value(v):
     if type(v) == type(0):
         return v
     elif type(v) == type(1.0):
         return v
     elif type(v) == type(""):
-        m = re.match(r"^(\d+)([.kupmM]?)(\d*)([kupmM]?)", v)
+        m = re.match(r"^(\d+)([.kunpmM]?)(\d*)([kunpmM]?)", v)
         if not m:
             return False
         if m.group(2) != '.' and m.group(2) != '' and m.group(4) != '':
             return False
+        mul = 0
         multiplier = m.group(2) if m.group(2) != '' and m.group(2) != '.' else m.group(4)
-        mul = 1.0
         if multiplier == 'M':
-            mul = 1000000.0
+            mul = 6
         elif multiplier == 'k':
-            mul = 1000.0
+            mul = 3
         elif multiplier == 'm':
-            mul = 0.001
+            mul = -3
         elif multiplier == 'u':
-            mul = 0.000001
+            mul = -6
+        elif multiplier == 'n':
+            mul = -9
         elif multiplier == 'p':
-            mul = 0.000000000001
+            mul = -12
         n = m.group(1)
         if m.group(2) != '':
             n += '.'
         n += m.group(3)
-        n = float(n) * mul
+        if mul != 0:
+            n += 'e' + str(mul)
         return n
     else:
         assert False
@@ -75,13 +78,14 @@ def get_rescapind(kind, opts, searchopts):
     append_generic_urlopts(opts, searchopts, urlopts)
 
     if opts.get('value') is not None:
-        urlopts.append(('filter[fields][specs.resistance.value][]', parse_value(opts['value'])))
+        urlopts.append(('filter[fields][specs.%s.value][]' % ance[kind], convert_value(opts['value'])))
     if opts.get('package') is not None:
         urlopts.append(('filter[fields][specs.case_package.value][]', opts['package']))
     if opts.get('tolerance') is not None:
         urlopts.append(('filter[fields][specs.%s_tolerance.value][]' % ance[kind], '±' + opts['tolerance']))
 
     url += urllib.parse.urlencode(urlopts)
+    print (url)
     data = urllib.request.urlopen(url).read()
     j = json.loads(data.decode('utf-8'))
     for r in j['results']:
@@ -96,4 +100,4 @@ def get_capacitor(opts, searchopts):
 def get_inductor(opts, searchopts):
     return get_rescapind('inductor', opts, searchopts)
 
-get_resistor(dict(value="10k", package="0402", rohs=True), dict(by='price'))
+get_capacitor(dict(value="10u", package="0402", rohs=True), dict(by='price'))
