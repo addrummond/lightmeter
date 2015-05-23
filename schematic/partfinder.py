bc9910ea30a478d88cc085b3b5126b77d85e01ea
Alex Drummond <a.d.drummond@gmail.com>
2015-05-23 16:47:46 -0400

    More on partfinder.py

--- schematic/partfinder.py
+++ schematic/partfinder.py
@@ -6,13 +6,15 @@ import json
 API_KEY = os.environ['OCTOPART_API_KEY']
 API_BASE_URL = """http://octopart.com/api/v3/"""
 
+ex= """'https://octopart.com/search?filter[fields][category_uids][]=5c6a91606d4187ad&filter[fields][specs.resistance.value][]=100000&filter[fields][specs.case_package.value][]=0402'"""
 def get_resistor():
     url = API_BASE_URL + 'parts/search?' + urllib.parse.urlencode([
         ('apikey', API_KEY),
         ('start', 0),
         ('limit', 10),
         ('filter[fields][category_uids][]', 'cd01000bfc2916c6'),
-        ('filter[value]', '100k')
+        ('filter[fields][specs.case_package.value][]', '0402'),
+        ('filter[fields][specs.resistance.value]', '10000')
     ])
     print(url)
     data = urllib.request.urlopen(url).read()
